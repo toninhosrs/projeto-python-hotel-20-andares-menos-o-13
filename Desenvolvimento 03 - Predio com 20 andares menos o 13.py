76602d5811f196c7898452c41f6cfba9937a74c7
Jose Antonio Soares <134891952+toninhosrs@users.noreply.github.com>
2023-09-19 22:27:06 -0300

    Add files via upload
    Arquivo projeto em Python

---
+++ Desenvolvimento 03 - Predio com 20 andares menos o 13.py
@@ -0,0 +1,16 @@
+for andar in range(1, 21):
+  if andar != 13:
+    print(andar)
+
+
+andar = 1
+while andar <= 20:
+  if andar != 13:
+    print(andar)
+  andar += 1
+
+
+for andar in range(20, 0, -1):
+  if andar != 13:
+    print(andar)
+
